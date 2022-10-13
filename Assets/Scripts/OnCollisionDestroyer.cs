720e3765fe8c7aa48a3cfd423fc0e960297d4125
GigaOrts <ortsortskhoev95@gmail.com>
2022-10-13 20:19:04 +0300

    code refactor

--- Assets/Scripts/OnCollisionDestroyer.cs
+++ Assets/Scripts/OnCollisionDestroyer.cs
@@ -2,21 +2,16 @@ using UnityEngine;
 
 public class OnCollisionDestroyer : MonoBehaviour
 {
-    [SerializeField] private Player playerTemp;
-
     private int onCollisionDamage = 1;
-    private int onCollisionScore = 1;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent(out Player player))
+        if (other.TryGetComponent(out Player player)) 
         {
-            player.TakeDamage(onCollisionDamage);
+            player.TakeDamage(onCollisionDamage); 
         }
-
-        if (other.TryGetComponent(out Projectile _))
+        else
         {
-            playerTemp.AddScore(onCollisionScore);
             Destroy(other.gameObject);
         }
 
